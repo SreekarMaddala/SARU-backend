34181b1b33925d67607d1ef614545a6b997cd6c8
Sreekar Maddala <maddalasreekar9999@gmail.com>
2025-09-18 23:46:59 +0530

    modified db

--- backend/database.py
+++ backend/database.py
@@ -3,17 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-
-
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 def get_db():
     db = SessionLocal()
     try:
